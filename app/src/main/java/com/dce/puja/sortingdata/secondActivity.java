ffcc558cec9462eb96c6e42a79a5a2d73ccd7f14
Shreya4net <pujakumari.civil@gmail.com>
2017-04-11 15:51:57 +0530

    android session4 assignment3
    android session4 assignment3

---
+++ app/src/main/java/com/dce/puja/sortingdata/secondActivity.java
@@ -0,0 +1,18 @@
+package com.dce.puja.sortingdata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.ImageView;
+public class secondActivity extends AppCompatActivity {
+    ImageView selectedImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+     selectedImage = (ImageView) findViewById(R.id.selectedImage);
+    Intent intent = getIntent(); // get Intent which we set from Previous Activity
+    selectedImage.setImageResource(intent.getIntExtra("image", 0)); // get image from Intent and set it in ImageView
+}
+}
+
